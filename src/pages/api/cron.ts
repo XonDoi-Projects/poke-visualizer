ba73be59265d7dcb2387d8393176cbee62b54dba
Nathan Magro <nathanmagro1993+1@gmail.com>
2024-10-25 16:08:34 -0500

    cron handler fix

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
-export async function GET(_req: Request) {
+export default async function GET(_req: Request) {
   // your db logic
   return NextResponse.json({
     result: "Cron ran successfully",
