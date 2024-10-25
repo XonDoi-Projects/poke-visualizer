4f0226c3ebf43acd56f71deb6d4a6143a7b3da3d
Nathan Magro <nathanmagro1993+1@gmail.com>
2024-10-25 17:40:58 -0500

    reattempt cron

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export const revalidate = 0;
 export const dynamic = "force-dynamic";
-export default function GET(_req: NextApiRequest, res: NextApiResponse) {
+export const cronTest = async (_req: NextApiRequest, res: NextApiResponse) => {
   // your db logic
   return res.status(200).send({ message: "Cron run successfully!" });
-}
+};
